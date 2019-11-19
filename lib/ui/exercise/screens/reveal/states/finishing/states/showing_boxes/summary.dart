eb2c01d77a50a006dd5847cfaa51337a9ccd7685
Patryk Wychowaniec <wychowaniec.patryk@gmail.com>
2019-11-19 22:25:23 +0100

    [ui:exercise] Refactor the `BoxesSummary` component.

--- lib/ui/exercise/screens/reveal/states/finishing/states/showing_boxes/summary.dart
+++ lib/ui/exercise/screens/reveal/states/finishing/states/showing_boxes/summary.dart
@@ -6,6 +6,7 @@ import 'package:optional/optional.dart';
 import '../../bloc.dart';
 
 class BoxesSummary extends StatelessWidget {
+  static const regularStyle = const TextStyle(fontSize: 16.0);
   static const highlightedStyle = const TextStyle(fontWeight: FontWeight.bold);
 
   @override
@@ -28,12 +29,8 @@ class BoxesSummary extends StatelessWidget {
       children: texts.map((texts) {
         return Padding(
           padding: const EdgeInsets.only(bottom: 18.0),
-
           child: RichText(
-            text: TextSpan(
-              children: texts,
-            ),
-
+            text: TextSpan(children: texts, style: regularStyle),
             textAlign: TextAlign.center,
           ),
         );
@@ -46,36 +43,35 @@ class BoxesSummary extends StatelessWidget {
     final targetBox = exercise.getCorrectAnswersBox();
 
     if (answers == 0) {
-      return Optional.empty();
+      return const Optional.empty();
     }
 
-    return Optional.of([
-      // "You have answered "
-      const TextSpan(text: 'Udało Ci się odpowiedzieć na '),
-
-      // "1 flashcard" / "%d flashcards"
+    final texts = [
+      TextSpan(text: 'Udało Ci się odpowiedzieć na '),
       TextSpan(
         text: inflector.pluralize(InflectorVerb.flashcard, InflectorCase.accusative, answers),
         style: highlightedStyle,
       ),
+      TextSpan(text: ' - '),
+    ];
 
-      // " - "
-      const TextSpan(text: ' - '),
+    if (targetBox.isPresent) {
+      texts.addAll([
+        TextSpan(text: (answers == 1) ? 'trafi ona do następnego pudełka' : 'trafią one do następnego pudełka'),
+        TextSpan(text: ': '),
+        TextSpan(text: targetBox.value.name, style: highlightedStyle),
+      ]);
+    } else {
+      texts.addAll([
+        TextSpan(text: (answers == 1) ? 'pozostanie ona w aktualnym pudełku' : 'pozostaną one w aktualnym pudełku'),
+      ]);
+    }
 
-      // "it will be moved to the next box: " / "they will be moved to the next box: "
-      TextSpan(
-        text: (answers == 1) ? 'trafi ona do następnego pudełka' : 'trafią one do następnego pudełka',
-      ),
-
-      // ": "
-      const TextSpan(text: ': '),
-
-      // "%s" (the target box's name)
-      TextSpan(text: targetBox.value.name, style: highlightedStyle),
-
-      // "."
-      const TextSpan(text: '.'),
+    texts.addAll([
+      TextSpan(text: '.'),
     ]);
+
+    return Optional.of(texts);
   }
 
   Optional<List<TextSpan>> describeInvalidAnswers(Exercise exercise) {
@@ -83,60 +79,33 @@ class BoxesSummary extends StatelessWidget {
     final targetBox = exercise.getInvalidAnswersBox();
 
     if (answers == 0) {
-      return Optional.empty();
+      return const Optional.empty();
     }
 
     final texts = [
-      // "You have not answered"
-      const TextSpan(text: 'Nie udało Ci się odpowiedzieć na'),
-
-      // " "
-      const TextSpan(text: ' '),
-
-      // "1 flashcard" / "%d flashcards"
+      TextSpan(text: 'Nie udało Ci się odpowiedzieć na '),
       TextSpan(
         text: inflector.pluralize(InflectorVerb.flashcard, InflectorCase.accusative, answers),
         style: highlightedStyle,
       ),
-
-      // " - "
-      const TextSpan(text: ' - '),
+      TextSpan(text: ' - '),
     ];
 
-    switch (targetBox.isPresent) {
-    // Case 1: Cards will be moved to the target box
-      case true:
-        if (answers == 1) {
-          // "it will be moved to the previous box"
-          texts.add(const TextSpan(text: 'trafi ona do poprzedniego pudełka'));
-        } else {
-          // "they will be moved to the previous box"
-          texts.add(const TextSpan(text: 'trafią one do poprzedniego pudełka'));
-        }
-
-        texts.addAll([
-          // ": "
-          const TextSpan(text: ': '),
-
-          // "%s" (the target box's name)
-          TextSpan(text: targetBox.value.name, style: highlightedStyle),
-        ]);
-
-        break;
-
-    // Case 2: Cards won't be moved anywhere
-      default:
-        if (answers == 1) {
-          // "it remain in current box"
-          texts.add(const TextSpan(text: 'pozostanie ona w aktualnym pudełku'));
-        } else {
-          // "they will remain in current box"
-          texts.add(const TextSpan(text: 'pozostaną one w aktualnym pudełku'));
-        }
+    if (targetBox.isPresent) {
+      texts.addAll([
+        TextSpan(text: (answers == 1) ? 'trafi ona do poprzedniego pudełka' : 'trafią one do poprzedniego pudełka'),
+        TextSpan(text: ': '),
+        TextSpan(text: targetBox.value.name, style: highlightedStyle),
+      ]);
+    } else {
+      texts.addAll([
+        TextSpan(text: (answers == 1) ? 'pozostanie ona w aktualnym pudełku' : 'pozostaną one w aktualnym pudełku'),
+      ]);
     }
 
-    // "."
-    texts.add(const TextSpan(text: '.'));
+    texts.addAll([
+      TextSpan(text: '.'),
+    ]);
 
     return Optional.of(texts);
   }
